bee19fd1c750d0c1cc6b8dab2bb07430ade4846e
lambert1129 <lambert1129@foxmail.com>
2019-06-07 10:24:14 +0800

    u

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -3,8 +3,13 @@ package com.github.hcsp.pet;
 public class Cat {
     public String name;
 
+    public Cat(String name) {
+        this.name = name;
+    }
+
     @Override
     public String toString() {
+
         return "Cat(" + name + ")";
     }
 }
