f98480ea0229a06e33b102e95c06f425084be57d
zzxx9426 <97177440@qq.com>
2019-06-11 18:27:07 +0800

    none

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -4,8 +4,6 @@ public class Cat {
     public String name;
 
     @Override
-    public String toString() {
-
-        return "Cat(" + name + ")";
+    public String toString() { return "Cat(" + name + ")";
     }
 }
