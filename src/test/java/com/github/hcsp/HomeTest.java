7832662865c91cf26ee0dd28eed091270eab1600
Bo Zhang <bo@gradle.com>
2019-06-15 18:43:06 +0800

    Update repo

--- src/test/java/com/github/hcsp/HomeTest.java
+++ src/test/java/com/github/hcsp/HomeTest.java
@@ -1,12 +1,11 @@
 package com.github.hcsp;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class HomeTest {
     @Test
     public void testNewCat() {
-        assertEquals("Cat(Black)", Home.newCat("Black").toString());
+        Assertions.assertEquals("Cat(Black)", Home.newCat("Black").toString());
     }
 }
