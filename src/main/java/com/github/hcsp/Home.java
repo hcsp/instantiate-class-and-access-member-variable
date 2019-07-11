77e02a6949933180e5291b32d56fb08d295dbea9
frank-0908 <2059326740@qq.com>
2019-07-11 23:01:44 +0800

    创建类的实例并修改其成员

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -4,15 +4,15 @@ import com.github.hcsp.pet.Cat;
 
 public class Home {
     public static void main(String[] args) {
-        System.out.println(newCat("White"));
+        System.out.println(newCat("66"));
     }
 
     public static Cat newCat(String name) {
         Cat cat1=new Cat();
-        cat1.name="White";
+        cat1.name=name;
         return cat1;
         // Create a new Cat instance, assign the name to its "name" member variable,
         // and return it here
-        // 在这里创建一个新的Cat实例，将其成员变量"name"设为参数所指定的名字，并返回之
+        // 在这里创建一个新的Cat实例，将其成员变量"name"设为参数所指定的值（将参数所代表的值赋予成员变量 ‘name’），并返回之
     }
 }
