5d5dfb18d7a6a3296c832577ce8d23e9e60f2d1b
frank-0908 <2059326740@qq.com>
2019-07-11 23:07:32 +0800

    创建类的实例并修改其成员

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -13,6 +13,6 @@ public class Home {
         return cat1;
         // Create a new Cat instance, assign the name to its "name" member variable,
         // and return it here
-        // 在这里创建一个新的Cat实例，将其成员变量"name"设为参数所指定的值（将参数所代表的值赋予成员变量 ‘name’），并返回之
+        // 在这里创建一个新的Cat实例，将其成员变量"name"设为参数所指定的名字，并返回之
     }
 }
