0d55a09a4bc7434d1086c0022d8bb28bc1b4fbc8
frank-0908 <2059326740@qq.com>
2019-07-11 23:59:21 +0800

    创建类的实例并修改其成员

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -4,7 +4,5 @@ public class Cat {
     public String name;
 
     @Override
-    public String toString() {
-        return "Cat(" + name + ")";
-    }
+    public String toString() {return "Cat(" + name + ")"; }
 }
