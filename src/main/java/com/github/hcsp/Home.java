ae56c193e2685530537fa48e41968d5904a13444
Mandy Tao <taoxiumin2016@gmail.com>
2019-07-16 21:00:46 +0800

    change to use variable name

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -12,7 +12,7 @@ public class Home {
         // and return it here
         // 在这里创建一个新的Cat实例，将其成员变量"name"设为参数所指定的名字，并返回之
         Cat kitty = new Cat();
-        kitty.name = "kitty";
+        kitty.name = name;
         return kitty;
 
     }
