34e7e22d74b8683ef173069bb7ee3c77344a3428
xuepan-summer <54898607+xuepan-summer@users.noreply.github.com>
2019-09-25 17:10:02 +0800

    新建实例，修改其成员变量 (#89)
    新建实例，修改其成员变量 (#89)

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -10,6 +10,10 @@ public class Home {
     public static Cat newCat(String name) {
         // Create a new Cat instance, assign the name to its "name" member variable,
         // and return it here
-        // 在这里创建一个新的Cat实例，将其成员变量"name"设为参数所指定的名字，并返回之
+        // 在这里创建一个新的Cat实例，将其成员变量"name"设为参数所指定的名字，并返回值
+        Cat cat1 = new Cat();
+        cat1.name = name;
+        return cat1;
+        //命名为newcat1会报错
     }
 }
