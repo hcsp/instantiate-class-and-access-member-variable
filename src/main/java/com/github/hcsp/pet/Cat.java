49531c8bbd37b491c778abd97d51bf2666b15cd7
Administrator <2433005622@qq.com>
2019-10-28 16:26:22 +0800

    创建一个新的Cat实例，将其成员变量"name"设为参数所指定的名字，并将其返回。

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -3,10 +3,6 @@ package com.github.hcsp.pet;
 public class Cat {
     public String name;
 
-    public Cat(String name) {
-        this.name = name;
-    }
-
     @Override
     public String toString() {
         return "Cat(" + name + ")";
