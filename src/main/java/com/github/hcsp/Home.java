ec5a3dabf1a24f811eed980e9092e575180bf74b
liuxing <1151879967@qq.com>
2019-11-05 10:31:51 +0800

    已创建类的实例，并修改其成员

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -5,11 +5,14 @@ import com.github.hcsp.pet.Cat;
 public class Home {
     public static void main(String[] args) {
         System.out.println(newCat("White"));
-    }
-
+                                           }
     public static Cat newCat(String name) {
-        // Create a new Cat instance, assign the name to its "name" member variable,
+       Cat cat2 = new Cat( );
+       cat2.name = "White";
+                return cat2;
+                                           }
+       // Create a new Cat instance, assign the name to its "name" member variable,
         // and return it here
         // 在这里创建一个新的Cat实例，将其成员变量"name"设为参数所指定的名字，并返回之
-    }
-}
+                    }
+
