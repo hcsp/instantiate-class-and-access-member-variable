5530033390cd4d67858022eb7987b87a13fd68bf
0x522 <chenyuntao1997@163.com>
2020-06-17 10:59:47 +0800

    创建一个Cat实例并设置名字，最后返回它。

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -3,6 +3,10 @@ package com.github.hcsp.pet;
 public class Cat {
     public String name;
 
+    public Cat(String name) {
+        this.name = name;
+    }
+
     @Override
     public String toString() {
         return "Cat(" + name + ")";
