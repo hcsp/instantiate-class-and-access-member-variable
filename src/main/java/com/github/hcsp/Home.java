a6816ef241f82e169ba532920d401681c649f4c5
giantpetter <65336996+giantpetter@users.noreply.github.com>
2021-04-18 10:38:47 +0800

    改变对象成员变量

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -11,5 +11,10 @@ public class Home {
         // Create a new Cat instance, assign the name to its "name" member variable,
         // and return it here
         // 在这里创建一个新的Cat实例，将其成员变量"name"设为参数所指定的名字，并返回之
+        Cat c = new Cat();
+        c.name = name;
+        return c;
+
+
     }
 }
