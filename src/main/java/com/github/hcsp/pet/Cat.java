7831490aa30d4b657fb90e420e5da23377a6b245
YJL <2567843704@qq.com>
2021-09-23 19:58:29 +0800

    2

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -2,9 +2,8 @@ package com.github.hcsp.pet;
 
 public class Cat {
     public String name;
-
     @Override
-    public String toString() {
+    public String toString(){
         return "Cat(" + name + ")";
     }
 }
