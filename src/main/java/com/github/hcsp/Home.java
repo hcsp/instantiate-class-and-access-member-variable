3afe79b8f23bc58ec6cf71b49d278419777c295c
iamxliu <94578246+iamxliu@users.noreply.github.com>
2022-03-24 20:49:40 +0800

    15 (#302)
    15 (#302)

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -5,11 +5,16 @@ import com.github.hcsp.pet.Cat;
 public class Home {
     public static void main(String[] args) {
         System.out.println(newCat("White"));
+
     }
+
 
     public static Cat newCat(String name) {
         // Create a new Cat instance, assign the name to its "name" member variable,
         // and return it here
         // 在这里创建一个新的Cat实例，将其成员变量"name"设为参数所指定的名字，并返回之
+       Cat c = new Cat();
+       c.name = name;
+       return c;
     }
 }
